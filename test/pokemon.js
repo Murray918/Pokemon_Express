38241efbdc55f9682e8217380e0ebea407a7ceb9
Ryan Fleharty <ryan.fleharty@gmail.com>
2018-10-09 17:45:37 -0600

    Fixed and added tests for part 2

--- test/pokemon.js
+++ test/pokemon.js
@@ -4,7 +4,7 @@ const app = require('../server');
 const should = chai.should()
 const cheerio = require('cheerio');
 
-describe('pokemon routes index and show', function(){
+describe('RESTful pokemon routes', function(){
     it('displays all pokemon at the index route', function(done){
         supertest(app)
             .get('/pokemon')
@@ -33,8 +33,78 @@ describe('pokemon routes index and show', function(){
         .expect(200)
         .expect((response)=>{
             const $ = cheerio.load(response.text)
-            const firstHref = $('.pokemon li:first-child').attr('href');
+            const firstHref = $('.pokemon li:first-child a').attr('href');
             firstHref.should.be.eq('/pokemon/0')
+        })
+        .end(done)
+    })
+    it('has an edit page', function(done){
+        supertest(app)
+        .get('/pokemon/0/edit')
+        .expect(200)
+        .expect((response)=>{
+            const $ = cheerio.load(response.text)
+            const formMethod = $('form').attr('method');
+            formMethod.should.be.eq('POST')
+        })
+        .end(done)
+    })
+    it('has a new pokemon page', function(done){
+        supertest(app)
+        .get('/pokemon/new')
+        .expect(200)
+        .expect((response)=>{
+            const $ = cheerio.load(response.text)
+            const formMethod = $('form').attr('method');
+            formMethod.should.be.eq('POST')
+            const formAction = $('form').attr("action");
+            formAction.should.be.eq('/pokemon')
+        })
+        .end(done)
+    })
+    it('can create a new pokemon with a POST request', function(done){
+        supertest(app)
+        .post('/pokemon')
+        .send({"name": "Morty"})
+        .end(()=>{
+            supertest(app)
+            .get('/pokemon')
+            .expect((response)=>{
+                const $ = cheerio.load(response.text);
+                $('body').text().includes("Morty");
+            })
+            .end(done)
+        })
+    })
+    it('can update a pokemon with a PUT request', function(done){
+        supertest(app)
+        .put('/pokemon/2')
+        .send({'name': 'The pokemon formerly known as Venusaur'})
+        .end(()=>{
+            supertest(app)
+            .get('/pokemon/2')
+            .expect(200)
+            .expect((response)=>{
+                const $ = cheerio.load(response.text);
+                $('body').text().includes("The pokemon formerly known as Venusaur").should.be.eq(true);
+                $('body').text().includes("Charizard").should.be.eq(false);
+            })
+            .end(done)
+        })
+    })
+    it('can delete a pokemon with a DELETE request', function(done){
+        supertest(app)
+        .delete('/pokemon/0')
+        .end(()=>{
+            supertest(app)
+            .get('/pokemon/0')
+            .expect(200)
+            .expect((response)=>{
+                const $ = cheerio.load(response.text);
+                $('body').text().includes("Ivysaur").should.be.eq(true);
+                $('body').text().includes("Bulbasaur").should.be.eq(false);
+            })
+            .end(done)
         })
     })
 })
