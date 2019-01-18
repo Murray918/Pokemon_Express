77751d6596dca1a901c20cec71f7aa4a3a1cd164
Jamal Amar Bahena <jamal@Jamals-MacBook-Pro.local>
2019-01-17 20:32:42 -0600

    My server is set up and running

---
+++ server.js
@@ -0,0 +1,19 @@
+
+const express = require('express')
+const app = express()
+
+
+
+
+
+
+
+
+
+
+
+const PORT = 3000
+
+app.listen(PORT, ()=> {
+	console.log('Pokemon-Express running on:', PORT)
+})
