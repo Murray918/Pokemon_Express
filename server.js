4f138ef287cd48404103392c680ffd1a3372c957
Jamal Amar Bahena <jamal@Jamals-MacBook-Pro.local>
2019-01-18 13:38:31 -0600

    index.ejs view rendered at pokemon route

--- server.js
+++ server.js
@@ -2,16 +2,19 @@
 const express = require('express')
 const pokemon = require('./models/pokemon')
 const app = express()
+const path = require('path')
+app.use(express.static(path.join(__dirname, 'public')))
 app.set('view engine', 'ejs')
 
 
 
-app.get('/', (req, res)=> {
-	res.render('pages/index')
-})
 
 app.get('/pokemon', (req, res)=> {
-	res.send(pokemon)
+	res.render('pages/index', {
+		data: pokemon,
+		page: 'list',
+		cssPath: 'css/style.css'
+	})
 })
 
 
