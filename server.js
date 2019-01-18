d8a54f8c7671303c2cbd7fdc6b6ff7cdfee13b9e
Tristan Bennett <tristanbennett@tristanova.local>
2019-01-17 18:13:04 -0600

    Commit 2 - Connected my database, can see data in the browser as our pokemon

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const pokemon = require('./models/pokemon.js')
 const PORT = 3000;
 
 let app = express()
+
+app.get('/pokemon', (request, response) => {
+	response.send(pokemon)
+})
 
 app.listen(PORT)
 console.log(`listening on port: ${PORT}`)
