1fea0ab928038dfe9a78f6aa342efae613c3ebc5
Jacob Werbin <jwerbin95@gmail.com>
2019-01-18 18:25:24 -0600

    Added new pokemon and edit pokemon pages

--- server.js
+++ server.js
@@ -4,9 +4,12 @@ const path = require('path');
 const bodyParser = require('body-parser');
 let app = express();
 let pokemon = require('./models/pokemon.js');
+let idChange = 0;
 
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'ejs');
@@ -14,8 +17,24 @@ app.set('view engine', 'ejs');
 app.get('/pokemon', (request, response) => {
 	response.render('pages/index.ejs', { pokemon: pokemon });
 });
+app.get('/pokemon/new', (request, response) => {
+	response.render('pages/new.ejs', { action: '/pokemon' });
+});
+app.get('/pokemon/:id/edit', (request, response) => {
+	idChange = request.params.id;
+	response.render('pages/edit.ejs', { action: '/pokemon/:id' });
+});
 app.get('/pokemon/:id', (request, response) => {
 	response.render('pages/show.ejs', { pokemon: pokemon[request.params.id] });
+});
+app.put('/pokemon/:id', (request, response) => {
+	pokemon[idChange].name = request.body['name'];
+	if (request.body['URL'] != '') pokemon[idChange].img = request.body['URL'];
+	response.redirect('/pokemon');
+});
+app.post('/pokemon', (request, response) => {
+	pokemon.push({ name: request.body['name'], img: request.body['URL'] });
+	response.redirect('/pokemon');
 });
 
 app.listen(port);
