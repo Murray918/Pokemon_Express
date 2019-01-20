a15d41d93c81eb1e7e86248f044814ad097fb2ca
Jamal Amar Bahena <jamal@Jamals-MacBook-Pro.local>
2019-01-19 23:41:33 -0600

    working css path

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 
-const express = require('express')
-const pokemon = require('./models/pokemon')
-const app = express()
-const path = require('path')
-app.use(express.static(path.join(__dirname, 'public')))
-app.set('view engine', 'ejs')
+const express = require('express');
+const pokemon = require('./models/pokemon');
+const app = express();
+const path = require('path');
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
 
 
 
