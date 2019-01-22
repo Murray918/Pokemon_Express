f7e05117a1d0cffcb0988f8dc2b9f3f06ac4d6fe
Tristan Bennett <tristanbennett@tristanova.local>
2019-01-21 21:38:43 -0600

    inex.ejs view rendered at pokemon route

--- server.js
+++ server.js
@@ -4,13 +4,18 @@ const path = require('path')
 const PORT = 3000
 
 let app = express()
+
+// this links our server to a static public directory
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.set('views', './views')
 app.set('view engine', 'ejs')
 
 app.get('/pokemon', (request, response) => {
 	response.render('pages/index.ejs', {
 		component: pokemon,
-		page: '../partials/list.ejs'
+		page: '../partials/list.ejs',
+		cssPath: 'css/style.css'
 	})
 })
 
