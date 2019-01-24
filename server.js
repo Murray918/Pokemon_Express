e11a54bcba99cd388967e131a2620e96c9e34431
Andrew Aluko <aluko17@Andrews-MacBook-Air-3.local>
2019-01-24 13:49:02 -0600

    Commit 2 - Connected my database, can see data in the browser as our pokemon

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 //dependencies 
 
-const pokemon = require('./models/pokemon')
+const pokemon = require('./pokemon')
 const express = require('express')
 let app = express()
-
+const ejs = require ('ejs')
+app.set('views', './views' )
+app.set('view engine', 'ejs')
 //PORT
 const PORT = 3000;
 
@@ -14,15 +16,27 @@ app.listen(PORT, function () {
 console.log('POKEMON GO')
 })
 
+//read all route
+app.get('/pokemon', (req, res)=>{
+	
+	
+
+	res.send(pokemon)
+})
+
+
 
 //index route
-app.set('view engine', 'ejs')
 
-app.get('/', (req, res)=>{
-	res.send(     {
-		thing: 'pokemon'
 
-	})
+app.get('/pokemon/:index', (req, res)=>{
+	
+	let index = req.params.index
+
+	res.send(pokemon[index])
+		
+
+	
 })
 
 
