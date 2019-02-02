9ba28b407194a371ff6f0031163070b279e95ef5
Sithiz <Joshklaas1@gmail.com>
2019-02-02 15:39:08 -0600

    Most parts working need to revist getting new pokemon to post

--- server.js
+++ server.js
@@ -1,26 +1,39 @@
 const express = require('express')
 const app = express()
 const pokemon = require('./pokemon.js')
+const ejs = require('ejs')
+const bodyparser = require('body-parser')
+const PORT = 9000
 
-
-app.set('view', './view')
+app.set('views', './view')
 app.set('view engine', 'ejs')
 
 
+app.use(bodyparser.urlencoded(false))
+app.use(bodyparser.json())
 
 
 
 
-
-
-
-
-
-
-
-app.get('/',(request,response)=>{
-	response.send(pokemon[0].img)
+app.get('/pokemon',(request,response)=>{
+	response.render('index.ejs', {
+		poke: pokemon,
+		page: 'list.ejs'
+	})
+})
+app.get('/pokemon/:id', (request,response)=>{
+	response.render('index.ejs',{
+		poke: pokemon[request.params.id],
+		page: 'show.ejs'
+	})
 })
 
+app.get('/new',(request,response)=>{
+	response.render('enter.ejs',{
+		page: 'enter.ejs'
+	})
+})
 
-app.listen(3000)
+app.listen(PORT, ()=>{
+	console.log('LISTENING AT PORT:' + PORT)
+})
