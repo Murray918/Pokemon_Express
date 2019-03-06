b51c2409c9e56a853ad5bf29aa09bb5992641400
Julius Camarillo <julcamarillo17@gmail.com>
2019-03-05 18:35:28 -0600

    show view shows poke details

--- server.js
+++ server.js
@@ -1,19 +1,36 @@
 const express = require('express')
 
 let app = express()
+const ejs = require ('ejs')
 
 app.set('view engine','ejs')
+app.set('views', './views')
 
-const port = 3000
+const port = 4000
 
 pokemon = require('./models/pokemon')
+const pokemon = require('./pokemon')
 
-app.get('/',(request,response)=>{
-	response.send(pokemon)
+app.get('/pokemon', (req, res)=>{
+	res.render('index.ejs', {
+		pkmn: pokemon,
+		page: 'list.ejs'
+	})	
 })
 
-
-
+//index
+app.get('/pokemon/:index', (req, res)=>{
+	let index = req.params.index
+	res.send(pokemon[index])
+	res.render('index.ejs',{
+		pkmn: pokemon[index],
+		page: 'show.ejs'
+	})
+})
+// app.post('/pokemon/:id',(request,respond)=>{
+// 	let id = request.params.id
+// 	response.send()
+// })
 
 app.listen(port, function(){
 	console.log('Pokemon running on port', port);
